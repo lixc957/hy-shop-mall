b008379d725a53ff929d927edbc20384552325fa
lxc <871673791@qq.com>
2020-08-16 12:05:09 +0800

    路径别名配置

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-<<<<<<< HEAD
         components: "@/components",
         content: "components/content",
         common: "components/common",
@@ -13,15 +12,4 @@ module.exports = {
     },
   },
 };
-=======
-        'components': '@/components',
-        'content': 'components/content',
-        'common': 'components/common',
-        'assets': '@/assets',
-        'network': '@/network',
-        'views': '@/views'
-      }
-    }
-  }
-}
->>>>>>> 19ec26bc3cd7817ef6af4b3290bec9bb10f482b5
+
