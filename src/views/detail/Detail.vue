f43e3c5da87fea36d4bca2ac6dd0f58aeb50a78d
lxc <871673791@qq.com>
2020-08-18 20:15:06 +0800

    商品基本信息渲染

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
 
-import {getDetail} from 'network/detail'
+import {getDetail,Goods} from 'network/detail'
 
 export default {
   name: 'Detail',
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     }
   },
   created() {
@@ -25,12 +28,19 @@ export default {
     //请求商品数据
     getDetail(this.iid).then(res => {
       console.log(res)
-      this.topImages = res.result.itemInfo.topImages
+      const data = res.result
+
+      //1.获取顶部轮播图信息
+      this.topImages = data.itemInfo.topImages
+
+      //2.获取商品信息
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
     })
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   }
 }
 </script>
