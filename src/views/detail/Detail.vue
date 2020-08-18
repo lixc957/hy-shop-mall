9a40f0d796a6e3ef1ca278d187d9ea3d4d4a5c65
lxc <871673791@qq.com>
2020-08-18 18:30:02 +0800

    详情页轮播图渲染

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="detail">
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
+import DetailSwiper from './childComps/DetailSwiper'
 
 import {getDetail} from 'network/detail'
 
 export default {
+  name: 'Detail',
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   created() {
@@ -21,10 +25,12 @@ export default {
     //请求商品数据
     getDetail(this.iid).then(res => {
       console.log(res)
+      this.topImages = res.result.itemInfo.topImages
     })
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   }
 }
 </script>
