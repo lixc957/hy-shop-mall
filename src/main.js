2c83b69a14eddbcfd19334a6d6cd8d0264f5963c
lxc <871673791@qq.com>
2020-08-24 17:55:01 +0800

    解决移动端点击300ms延迟

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import store from './store'
 
 import toast from 'common/toast'
 
+import FastClick from 'fastclick'
+
 Vue.config.productionTip = false
 
 // 添加事件总线对象
 Vue.prototype.$bus = new Vue()
 // 安装 toast 插件
 Vue.use(toast)
+// 解决移动端点击300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
   router,
