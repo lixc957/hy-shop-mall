b6034615ed278475a58be862087e1bd0373d1e65
lxc <871673791@qq.com>
2020-08-28 16:38:28 +0800

    toasst插件路径更改

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import toast from 'common/toast'
 import './plugins'
 
 Vue.config.productionTip = false
 
 // 添加事件总线对象
 Vue.prototype.$bus = new Vue()
-// 安装 toast 插件
-Vue.use(toast)
 
 new Vue({
   router,
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,8 @@
+import Vue from 'vue'
+import toast from './toast'
 import './fastclick'
 import './vue-lazyload'
 import 'assets/js'
+
+// 安装 toast 插件
+Vue.use(toast)
