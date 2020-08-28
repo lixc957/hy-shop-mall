323f5ea3996a77961a22f034ce6cf83cfc25a35a
lxc <871673791@qq.com>
2020-08-28 18:16:16 +0800

    profile页面完成

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -22,22 +22,31 @@
         <div class="account-info">我的积分</div>
       </div>
     </div>
-
+    
+    <list-view :list-data="orderList" class="order-list" />
   </div>
 </template>
 
 <script>
   import UserInfo from './childComps/UserInfo'
+  import ListView from './childComps/ListView'
 
   export default {
     name: "Profile",
     data () {
       return {
-
+        orderList: [
+          {icon: '#order', iconColor: '#ff8198', info: '我的消息'},
+          {icon: '#point', iconColor: '#fc7b53', info: '积分商城'},
+          {icon: '#vip', iconColor: '#ffc636', info: '会员卡'},
+          {icon: '#service', iconColor: '#ff8198', info: '我的购物车'},
+          {icon: '#download', iconColor: '#ff8198', info: '下载购物APP'},
+        ]
       }
     },
     components: {
-      UserInfo
+      UserInfo,
+      ListView
     }
   }
 </script>
@@ -78,6 +87,10 @@
 
   .account-info {
     margin-top: 6px;
+  }
+
+  .order-list, .service-list {
+    margin-top: 12px;
   }
 
 </style>
